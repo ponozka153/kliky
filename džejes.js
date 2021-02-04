146592a70bf5da8bc24e04feb92b9014439ad575
ponozka153 <michalhoracek88@gmail.com>
2021-02-04 19:17:36 +0100

    Jez

--- džejes.js
+++ džejes.js
@@ -1,22 +1,10 @@
-const ajpí = fetch("http://api.ipify.org/?format=json").then(results => results.json())
-
-function ano() {
-    //ajpí.then(data => alert(data.ip))
-
-    //if(ajpí.then(data => data.ip === "")){
-//ajpí.then(data => localStorage.setItem("key", data.ip))
-//localStorage.setItem("hezů", "hezů")
-//document.getElementsByClassName("bu")[0].innerHTML = localStorage.getItem("key")}
-//else{document.getElementsByClassName("bu")[0].innerHTML = "Ahuuuj"}
-}
 
 function ne() {
     Number1 = Math.floor (Math.random() * (10))
-    fetch("file:///C:/Users/micha/OneDrive/Documents/GitHub/kliky/hez%C5%AF.json").then(results => document.getElementsByClassName("bubu")[0].innerHTML = results.json())
     if(Number1 > 1){
     document.getElementsByClassName("nadpis")[0].innerHTML = "Klikyyy"
 }
-    if(Number1 === 1){
+    if(Number1 === 1){s
         document.getElementsByClassName("nadpis")[0].innerHTML = "Zmáčkni mě 🥰"
     }
 }
